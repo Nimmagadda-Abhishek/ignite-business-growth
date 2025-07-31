0bb55120339f79d10849266d4987980c8004b2e0
Abhishek <116945459+Nimmagadda-Abhishek@users.noreply.github.com>
2025-07-31 20:37:58 +0530

    update

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ignite-business-growth-yqjx.vercel.app/",
+  baseURL: "https://ignite-business-growth-v2uv.vercel.app/",
 });
 
 export default api;
